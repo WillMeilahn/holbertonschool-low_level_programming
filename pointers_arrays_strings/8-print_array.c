75de31c7c83e3244ea214394769e59caebbf588c
WillMeilahn <meilahna@gmail.com>
2023-06-05 11:13:46 -0700

    8 test

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+/*
+ * File: 8-print_array.c
+ * Auth: William A Meilahn
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Prints an inputed number of elements
+ * 		 of an array of integers.
+ * @a: The array of integers.
+ * @n: The number of elements to be printed.
+ */
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+
+		if (index == n)
+			continue;
+
+		printf(", ");
+	}
+}

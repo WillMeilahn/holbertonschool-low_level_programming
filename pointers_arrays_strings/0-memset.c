1759e794f3740dcb7ab9cd81f4e4527c418471c2
WillMeilahn <meilahna@gmail.com>
2023-06-09 09:14:39 -0700

    0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * The following code fills memory with a constant byte.
@@ -9,8 +8,7 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned char *p = (unsigned char *)s;
 
 	while (n--)
-	{
 		*p++ = (unsigned char)b;
-	}
+
 	return (s);
 }
